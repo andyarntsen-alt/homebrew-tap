bb4742f82ba15d0fafc7d5bff0c72ab0d6d3f782
Andreas Arntsen <andreasarntsen@Andreas-sin-MacBook-Pro.local>
2026-03-22 23:33:43 +0100

    Update kviskr cask to 0.1.2

--- Casks/kviskr.rb
+++ Casks/kviskr.rb
@@ -1,6 +1,6 @@
 cask "kviskr" do
-  version "0.1.1"
-  sha256 "d9f61ba3a640d11c4068f5b849ab97f7ec1d6d2ed8e1d0b6b9cd5d95f822d468"
+  version "0.1.2"
+  sha256 "07097433e41609db253e33291757cb944592d9a5b88d2d7db936699080b7bac5"
 
   url "https://github.com/andyarntsen-alt/kviskr-downloads/releases/download/v#{version}/Kviskr.dmg"
   name "Kviskr"
