a5e671a34c9b5f3ca639bcf529f4780a804fa8d8
Andreas Arntsen <andreasarntsen@Andreas-sin-MacBook-Pro.local>
2026-03-26 16:41:49 +0100

    Bump Kviskr til 0.3.1

--- Casks/kviskr.rb
+++ Casks/kviskr.rb
@@ -1,6 +1,6 @@
 cask "kviskr" do
-  version "0.3.0"
-  sha256 "1cedeafeccbb89f63671db7008e5e1d11d40f77239dc1ad75b329e3fe521bcb8"
+  version "0.3.1"
+  sha256 "1c47f916b7222d41598f2a4ecf536b290f718d1478ac6ca8f2662a4072947cf7"
 
   url "https://github.com/andyarntsen-alt/kviskr-downloads/releases/download/v#{version}/Kviskr.dmg"
   name "Kviskr"
