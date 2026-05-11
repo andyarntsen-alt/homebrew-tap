453646013bafab32355044ce035873e72240a3fa
Andreas <s.arntsen@yahoo.com>
2026-05-11 15:05:53 +0200

    Revert "Update kviskr cask to v0.5.0"
    This reverts commit 88f25a4720d7c8cc4d0d684498c4db2a99d039d7.

--- Casks/kviskr.rb
+++ Casks/kviskr.rb
@@ -1,6 +1,6 @@
 cask "kviskr" do
-  version "0.5.0"
-  sha256 "9d75d04dcc30ebc4744d0f531edf0aa22c226b936f5859637658748dd301b34e"
+  version "0.4.2"
+  sha256 "0256cd7648f2b096002d46bbed1f1c59332aa9be11c9aa702075ad817b0e774c"
 
   url "https://github.com/andyarntsen-alt/kviskr-downloads/releases/download/v#{version}/Kviskr.dmg"
   name "Kviskr"
