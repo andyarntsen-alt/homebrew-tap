1c2bdb753bf67f538829a4de42378f4b8760c7ac
Andreas <s.arntsen@yahoo.com>
2026-05-11 16:22:16 +0200

    Update kviskr cask to v0.5.0

--- Casks/kviskr.rb
+++ Casks/kviskr.rb
@@ -1,6 +1,6 @@
 cask "kviskr" do
-  version "0.4.2"
-  sha256 "0256cd7648f2b096002d46bbed1f1c59332aa9be11c9aa702075ad817b0e774c"
+  version "0.5.0"
+  sha256 "c1d0803ed24da27c777fa9211c28baaafe823cda608358f3a70d1f39f9d54bc0"
 
   url "https://github.com/andyarntsen-alt/kviskr-downloads/releases/download/v#{version}/Kviskr.dmg"
   name "Kviskr"
